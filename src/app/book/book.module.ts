83b8bbe56abcc7a3729291373c3c4d61d62fe55c
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-05 16:49:19 +0700

    add Book and Author component

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
 import { BookComponent } from './book.component';
-
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { BookComponent } from './book.component';
   ],
   imports: [
     CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
+    NgbDatepickerModule,
   ]
 })
 export class BookModule { }
